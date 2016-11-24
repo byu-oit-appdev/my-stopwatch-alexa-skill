dadcb01b9642e73873981272b4c35f999a53746f
Paul D Eden <paul.eden@byu.edu>
2016-11-24 03:18:12 +0000

    added better voice interactions

--- app.py
+++ app.py
@@ -31,6 +31,21 @@ app.config['ASK_APPLICATION_ID'] = 'amzn1.ask.skill.1925fb17-06d3-4803-b3a9-a73f
 app.config['ASK_VERIFY_REQUESTS'] = True # verify requests are coming from Alexa
 sdb = boto3.client('sdb')
 
+#### intent functions ####
+
+@ask.intent('AMAZON.YesIntent')
+def yes():
+    if session.attributes['question'] == 'restart?':
+        return restart()
+    elif session.attributes['question'] == 'start?':
+        return start()
+    else:
+        return start()
+
+@ask.intent('AMAZON.NoIntent')
+def no():
+    return statement('')
+
 @ask.launch
 @ask.intent('StartIntent')
 def start():
@@ -39,7 +54,15 @@ def start():
         text = 'S. Watch started.'
         return statement(text).simple_card('Started', text)
     else:
-        return statement('S. Watch is running and has already elapsed {}.'.format(get_current_duration()))
+        session.attributes['question'] = 'restart?'
+        return question('S. Watch is running and has already elapsed {}. Would you like to restart it?'.format(get_current_duration()))
+
+@ask.intent('RestartIntent')
+def restart():
+    stop_stopwatch()
+    start_stopwatch()
+    text = 'S. Watch restarted.'
+    return statement(text).simple_card('Re-Started', text)
 
 @ask.session_ended
 @ask.intent('StopIntent')
@@ -50,7 +73,8 @@ def stop():
         text = 'S. Watch ended.  {} have elapsed.'.format(duration)
         return statement(text).simple_card('Stopped', text)
     else:
-        return statement("S. Watch isn't running.")
+        session.attributes['question'] = 'start?'
+        return question("S. Watch isn't running. Would you like to start it?")
 
 @ask.intent('StatusIntent')
 def status():
@@ -59,7 +83,8 @@ def status():
         text = 'S. Watch running.  {} have elapsed.'.format(duration)
         return statement(text).simple_card('Status', text)
     else:
-        return statement("S. Watch isn't running.")
+        session.attributes['question'] = 'start?'
+        return statement("S. Watch isn't running. Would you like to start it?")
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
@@ -75,6 +100,8 @@ def stop():
         return statement(text).simple_card('Canceled', text)
     except KeyError:
         return statement("S. Watch isn't running.")
+
+#### action functions ####
 
 def stopwatch_started():
     result = sdb.get_attributes(DomainName='s-watch', ItemName=session.user.userId, ConsistentRead=True)
